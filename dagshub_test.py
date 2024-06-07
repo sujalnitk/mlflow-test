449a8565148fa0136a6191a445b7993584e60b01
GhostBYte <sksujaliitg@gmail.com>
2024-06-07 13:02:28 +0530

    added dagshub remote server

--- dagshub_test.py
+++ dagshub_test.py
@@ -13,6 +13,9 @@ import mlflow
 import mlflow.sklearn
 from mlflow.models import infer_signature
 
+import dagshub
+dagshub.init(repo_owner='sksujaliitg', repo_name='mlflow-test', mlflow=True)
+
 logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 
@@ -71,6 +74,9 @@ if __name__ == "__main__":
 
         predictions = lr.predict(train_x)
         signature = infer_signature(train_x, predictions)
+        
+
+        
 
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
